ca1ddbc042b154b1e4e6b20facd682454a6f66aa
moose-hub <infamollagames@gmail.com>
2024-02-27 12:22:17 +0000

    Ticket 2b, completed but test not working

--- quote.js
+++ quote.js
@@ -1,11 +1,21 @@
 import fs from "node:fs/promises";
 import { v4 as uuidv4 } from "uuid";
+import { readFile } from 'node:fs/promises';
 
 const fileName = "quotes.json";
 
 export async function getQuotes() {
-
+    try {
+        const data = await readFile(fileName, 'utf8');
+        const quotes = await JSON.parse(data);
+        return quotes;
+    }
+    catch (error) {
+        console.error(`Error reading filename ${fileName}:`, error)
+    }
 }
+
+console.log(await getQuotes());
 
 export async function addQuote(quoteText) {
 
